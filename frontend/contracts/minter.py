c9cfa84f6eebb70afe16dead373dbf80805e758b
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-04-07 08:42:15 -0400

    e2e

--- frontend/contracts/minter.py
+++ frontend/contracts/minter.py
@@ -5,50 +5,63 @@ from algosdk.v2client import algod
 import hashlib
 import json
 
-token = "a" * 64
-host = "http://localhost:4001"
-client = algod.AlgodClient(token, host)
+network = "testnet"
+
+ALGOD_ADDRESS = "http://localhost:4001"
+ALGOD_TOKEN = "a" * 64
+
+if network == "testnet":
+    ALGOD_ADDRESS = "https://testnet-api.algonode.cloud"
+    ALGOD_TOKEN = ""
+elif network == "mainnet":
+    ALGOD_ADDRESS = "https://mainnet-api.algonode.cloud"
+    ALGOD_TOKEN = ""
+
+client = algod.AlgodClient(ALGOD_TOKEN, ALGOD_ADDRESS)
 
 configs = [
     {
         "file": "Boiish - Nothin fancy ft. C-240 by Yucef Merhi",
-        "img": "bafybeibdtybsjf22k6nzw4cezyqycxzgt2uyviqnavm4ytzhbecbhrm2jm",
+        "img": "bafybeifwznzkdlrqozfhbinlolyuqjc46eohnl5tiucx6xhkjdpnnlwhyy",
         "integ":"GbQerxFPAqPBdxlR3XDL8P51WOB+TDM5nBEZEl4Oivw=",
-        "animation": "bafybeigmc3jau5vwvbeipib5vwdsj5ofgatxtcfek6kukopyzs45gjseme",
-        "md":"bafybeibrrcgy7m2tyz5brpc5r3w4edgtknf25o3v5dbjuqeaogmpld25cy",
+        "animation": "bafybeihq2gf325d2sjagk3t5zj4at2jdkucgcvxufzuuxrdw244forzme4",
+        "md":"bafkreiebpsp7rzfexxek6kn4ifwwy46atf5ujpg5eawtsdulbzpsaecdka",
         "sounds": "Boiish",
-        "unit":"240-1"
+        "unit":"C-240-NF",
+        "name":"C-240-ft-Nothin_Fancy",
+        "desc":"C-240 is an electronic sound artwork developed by digital art pioneer, Yucef Merhi, that translates sounds into colorful geometric patterns. This piece is a generative visual response to the song, Nothin Fancy, made by Boiish."
     },
     {
         "file": "Jonny From Space - Full Glass ft. C-240 by Yucef Merhi",
-        "img": "bafybeig2525pjxnxnq2j66x6phjrqtedmbkhy5bhemzqi223rnboixgfba",
+        "img": "bafybeiennx3b6ft7lvricepsbc4rq5rg7vqpoyha442neu3um5cfanyz5i",
         "integ":"NhSPyAyJSe0B4t+hFVUlEr0bwIOo4AKxQJfbU3MDfQ8=",
-        "animation": "bafybeif6p4jtickpn2eogd4qgygn24j7fb5dktptwdvdzhwwtplxfd3lb4",
-        "md":"bafybeicflbrbnyqvjjqcl4id7irkc322r42job6vntjcaapi4aam75s6yu",
+        "animation": "bafybeidt7qyretlhbkvhvjekzz4mwdrbmj354dl5l6nohrnzpeqxwbvtti",
+        "md":"bafkreihrwdhvstfzxi3zqrxyl6ijgz7uo3sozb6gdcu2jiezzpswu3tosy",
         "sounds":"Jonny From Space",
-        "unit":"240-2"
+        "unit":"C-240-FG",
+        "name":"C-240-ft-Full_Glass",
+        "desc":"C-240 is an electronic sound artwork developed by digital art pioneer, Yucef Merhi, that translates sounds into colorful geometric patterns. This piece is a generative visual response to the song, Full Glass, made by Jonny from Space."
     },
     {
         "file": "Nick-Leon - A Bailar ft. C-240 by Yucef Merhi",
-        "img": "bafybeiarxp4l6jjd3ogitl5jf3wwgopuad42mhirmwceht6ec4qwvae45u",
+        "img": "bafybeib6wu5myb7gh64my7vzffbpg6m4ueo53x7vrvkr65sl724uxpvuym",
         "integ":"/rDsfJnJKfREvnwEycLteGzcCLGz5VcWIjrMwaQBhM4=",
-        "animation": "bafybeigpaj64hpjgsz46gfykpazqnobaiqrbpbqjmld5wz35bny4exlwfy",
-        "md":"bafybeihbjhnwk57oymowqrazdcvfft6amjsfbmvsmhma5taz72gb36w4na",
+        "animation": "bafybeibzxuya53egqt54lcepd4arlrddrbvvi7oiew462v4cfap36glqlm",
+        "md":"bafkreigrtpoebspuugixc24zonvqmqy2n72msktdn7tuyapd5zo7fzer4i",
         "sounds":"Nick León",
-        "unit":"240-3"
+        "unit":"C-240-AB",
+        "name":"C-240-ft-A_Bailar",
+        "desc":"C-240 is an electronic sound artwork developed by digital art pioneer, Yucef Merhi, that translates sounds into colorful geometric patterns. This piece is a generative visual response to the song,  A Bailar, made by Nick Leon."
     }
 ]
 
 
 total = 88 
-asset_name = "CCxC-240"
-description = "Dequency Presents: Crypto Cabana x C-240"
-
 
 def mint(minter, cfg):
     metadata = {
-        "name": asset_name,
-        "description": description,
+        "name": cfg["name"],
+        "description": cfg["desc"],
         "image": "ipfs://{}".format(cfg['img']),
         "decimals": 0,
         "supply": total,
@@ -56,7 +69,7 @@ def mint(minter, cfg):
         "image_integrity": "sha256-{}".format(cfg['integ']),
         "image_mimetype": "image/jpg",
         "animation_url":"ipfs://{}".format(cfg['animation']),
-        "animation_mimetype":"video/mp4",
+        "animation_url_mimetype":"video/mp4",
         "properties": {
             "visuals": "C-240 by Yucef Merhi" ,
             "sounds":cfg["sounds"]
@@ -75,7 +88,7 @@ def mint(minter, cfg):
         sp,
         total=total,
         unit_name=cfg['unit'],
-        asset_name=asset_name,
+        asset_name=cfg["name"],
         manager=minter,
         url="ipfs://{}".format(cfg['md']),
         metadata_hash=mdhash,
@@ -87,13 +100,18 @@ if __name__ == "__main__":
     from deploy import get_accounts 
 
     accts = get_accounts()
-    (minter, sk) = accts[0]
+    minter = "MIAMI2UTQBFMGLX6PPTC456Q62NCPW5Q7SPVCXNMLKHKFNJWP5CBERUWZQ"
+
+    for (acct, sk) in accts:
+        if acct == "MIAMI2UTQBFMGLX6PPTC456Q62NCPW5Q7SPVCXNMLKHKFNJWP5CBERUWZQ":
+            print(acct)
+            key = sk 
 
     acfgs = []
     for c in configs:
         acfgs.append(mint(minter, c))
 
 
-    stxns = [a.sign(sk) for a in transaction.assign_group_id(acfgs)]
+    stxns = [a.sign(key) for a in transaction.assign_group_id(acfgs)]
 
-    client.send_transactions(stxns)
+    txid = client.send_transactions(stxns)
